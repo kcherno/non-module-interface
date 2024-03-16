d95680eb851e8914c2a65fa7470e15c282973fbd
Kirill Chernigovskiy <138433730+kcherno@users.noreply.github.com>
2024-03-16 20:09:25 +0800

    Add function header protection

--- non-module-interface.sh
+++ non-module-interface.sh
@@ -17,7 +17,26 @@ function filter
     sed -E '/^module[[:space:]]+:[[:space:]]+private[[:space:]]+;$/d'
 }
 
+function include
+{
+    if [ -n "${2}" ]; then
+	sed --in-place \
+	    '/.*/{H;$!d}
+	    ; x
+	    ; s:^:#ifndef __GUARD__\n#define __GUARD__\n:
+	    ; s:$:\n\n#endif // __GUARD__:' \
+	    ${1}
+
+	sed --in-place "s:__GUARD__:${2}:" ${1}
+
+    else
+	sed --in-place '/.*/{H;$!d} ; x ; s:^:#pragma once\n:' ${1}
+	
+    fi
+}
+
 INPUT=$1
 OUTPUT=$2
+GUARD=$3
 
-cat ${INPUT} | filter > ${OUTPUT}
+cat ${INPUT} | filter > ${OUTPUT} && include ${OUTPUT} ${GUARD}
